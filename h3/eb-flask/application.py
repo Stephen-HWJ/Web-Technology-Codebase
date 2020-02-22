a81bd0438a2641761ec9969117363af77499f284
Weiji Huang <weijihua@usc.edu>
2020-02-21 23:05:31 -0800

    add two restful apis for search page in backend

--- h3/eb-flask/application.py
+++ h3/eb-flask/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from processing import headlines
+from processing import headlines, get_sources, get_everything
 
 application = Flask(__name__)
 
@@ -17,7 +17,20 @@ def send_headlines():
 
 @application.route('/search')
 def search():
-    return jsonify({'username': request.args.get('username'), 'passwd': request.args.get('password'), 'new': request.args.get('new')})
+    keyword = request.args.get('keyword')
+    from_date = request.args.get('from')
+    to_date = request.args.get('to')
+    category = request.args.get('category')
+    source = request.args.get('source')
+    return jsonify({'everything': get_everything(q=keyword, from_param=from_date, to=to_date, sources=source)})
+
+
+@application.route('/sources')
+@application.route('/source')
+def send_source():
+    category = request.args.get('category')
+    news_sources = get_sources(category=category)
+    return jsonify({'sources': news_sources[:10]})
 
 
 # run the app.
