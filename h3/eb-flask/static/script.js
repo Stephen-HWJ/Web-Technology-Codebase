cbd8827b7fd63e0bbd4d382624cc0174141bb211
Weiji Huang <weijihua@usc.edu>
2020-03-06 12:34:54 -0800

    search result date format

--- h3/eb-flask/static/script.js
+++ h3/eb-flask/static/script.js
@@ -73,11 +73,12 @@ function getSource() {
 function getEveryDateFormat(dateStr) {
     const date = new Date(dateStr);
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    if (month < 10){
-        return "0" + month + "/" + day + "/" + year;
-    }
+    let month = date.getMonth() + 1;
+    if (month < 10)
+        month = "0"+ month;
+    let day = date.getDate();
+    if (day < 10)
+        day = "0" + day;
     return month + "/" + day + "/" + year;
 }
 
