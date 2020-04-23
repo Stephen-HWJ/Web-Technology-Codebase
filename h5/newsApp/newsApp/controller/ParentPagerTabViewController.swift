23adabdb53eb75f429cc9c6125869200c5aeb7c1
Weiji Huang <weijihua@usc.edu>
2020-04-23 15:44:35 -0700

    search in TabView done

--- h5/newsApp/newsApp/controller/ParentPagerTabViewController.swift
+++ h5/newsApp/newsApp/controller/ParentPagerTabViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 import XLPagerTabStrip
+import Alamofire
+import SwiftyJSON
 
-class ParentPagerTabViewController: ButtonBarPagerTabStripViewController {
+class ParentPagerTabViewController: ButtonBarPagerTabStripViewController, UISearchResultsUpdating {
     let purpleInspireColor = UIColor(red:0.13, green:0.03, blue:0.25, alpha:1.0)
     let redColor = UIColor(red: 221/255.0, green: 0/255.0, blue: 19/255.0, alpha: 1.0)
     let unselectedIconColor = UIColor(red: 73/255.0, green: 8/255.0, blue: 10/255.0, alpha: 1.0)
+    
+    /// Search controller to help us with filtering items in the table view.
+    var searchController: UISearchController!
+    
+    /// Search results table view.
+    private var resultsTableController: ResultsTableController!
+    
     override func viewDidLoad() {
         // change selected bar color
         settings.style.buttonBarBackgroundColor = .white
@@ -31,6 +40,32 @@ class ParentPagerTabViewController: ButtonBarPagerTabStripViewController {
             oldCell?.label.textColor = .lightGray
             newCell?.label.textColor = .systemBlue
         }
+        
+        resultsTableController =
+        self.storyboard?.instantiateViewController(withIdentifier: "ResultsTableController") as? ResultsTableController
+        // This view controller is interested in table view row selections.
+//        resultsTableController.tableView.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsTableController)
+        searchController.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+//        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self // Monitor when the search button is tapped.
+        
+        // Place the search bar in the navigation bar.
+        navigationItem.searchController = searchController
+        
+        /** Search presents a view controller by applying normal view controller presentation semantics.
+            This means that the presentation moves up the view controller hierarchy until it finds the root
+            view controller or one that defines a presentation context.
+        */
+        
+        /** Specify that this view controller determines how the search controller is presented.
+            The search controller should be presented modally and match the physical size of this view controller.
+        */
+        definesPresentationContext = true
+        
         super.viewDidLoad()
     }
     
@@ -50,7 +85,71 @@ class ParentPagerTabViewController: ButtonBarPagerTabStripViewController {
         child_4.childInfo = "SPORTS"
         child_5.childInfo = "TECHNOLOGY"
         child_6.childInfo = "SCIENCE"
-//        let child_2 = HomeTableViewController(style: .plain, childInfo: "table view 2")
         return [child_1, child_2, child_3, child_4, child_5, child_6]
     }
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let resultsController = searchController.searchResultsController as? ResultsTableController {
+            let suggestApi = "https://api.cognitive.microsoft.com/bing/v7.0/suggestions?q=\(searchController.searchBar.text!)"
+            let url = suggestApi.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            let headers: HTTPHeaders = ["Ocp-Apim-Subscription-Key": "fa5edb60ea4a419da391b8968bbfa824"]
+            
+            Alamofire.request(url!, headers: headers).responseJSON(completionHandler: {response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let resultArray = json["suggestionGroups"].arrayValue[0]["searchSuggestions"].arrayValue.map({$0["displayText"].stringValue})
+                    resultsController.searchResults = resultArray
+                    resultsController.tableView.reloadData()
+                case .failure(let error):
+                    print(error)
+                }
+            })
+            
+        }
+    }
+}
+
+
+// MARK: - UISearchBarDelegate
+
+extension ParentPagerTabViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+//        updateSearchResults(for: searchController)
+    }
+    
+}
+
+// MARK: - UISearchControllerDelegate
+
+// Use these delegate functions for additional control over the search controller.
+
+extension ParentPagerTabViewController: UISearchControllerDelegate {
+    
+    func presentSearchController(_ searchController: UISearchController) {
+        //Swift.debugPrint("UISearchControllerDelegate invoked method: \(#function).")
+    }
+    
+    func willPresentSearchController(_ searchController: UISearchController) {
+        //Swift.debugPrint("UISearchControllerDelegate invoked method: \(#function).")
+    }
+    
+    func didPresentSearchController(_ searchController: UISearchController) {
+        //Swift.debugPrint("UISearchControllerDelegate invoked method: \(#function).")
+    }
+    
+    func willDismissSearchController(_ searchController: UISearchController) {
+        //Swift.debugPrint("UISearchControllerDelegate invoked method: \(#function).")
+    }
+    
+    func didDismissSearchController(_ searchController: UISearchController) {
+        //Swift.debugPrint("UISearchControllerDelegate invoked method: \(#function).")
+    }
+    
 }
